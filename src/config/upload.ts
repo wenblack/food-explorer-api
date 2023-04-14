70a360cf79cd876f8f156fd5881440b64f151e89
wenblack <wenblack012@gmail.com>
2023-04-14 16:55:58 -0300

    ✖️ CreCreated Post route for bee Upload config file and install Multer ✖️

---
+++ src/config/upload.ts
@@ -0,0 +1,23 @@
+import path from "path"
+import multer from 'multer'
+import crypto from 'crypto'
+
+export const TMP_FOLDER = path.resolve(__dirname, "..", "..", "tmp")
+export const UPLOADS_FOLDER = path.resolve(__dirname, "uploads")
+
+export const Multer = {
+  storage: multer.diskStorage({
+
+    destination: TMP_FOLDER,
+    filename(req, file, callback) {
+      const fileHash = crypto.randomBytes(10).toString('hex')
+      const filename = `${fileHash}-${file.originalname}`
+
+      return (callback(null, filename))
+
+    },
+
+  })
+}
+
+
