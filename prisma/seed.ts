536f4e1288dd78692800fec5e9aa1431623f15ce
wenblack <wenblack012@gmail.com>
2023-04-14 15:13:21 -0300

    ⏯ Start project from template ⏯

---
+++ prisma/seed.ts
@@ -0,0 +1,54 @@
+import bcrypt from 'bcryptjs';
+import prisma from '../src/lib/prisma';
+
+async function main() {
+  await prisma.user.upsert({
+    where: { email: 'alice@prisma.io' },
+    update: {},
+    create: {
+      email: 'alice@prisma.io',
+      name: 'Alice',
+      password: bcrypt.hashSync('password', 8),
+      posts: {
+        create: {
+          title: 'Check out Prisma with Next.js',
+          content: 'https://www.prisma.io/nextjs',
+          published: true,
+        },
+      },
+    },
+  });
+
+  await prisma.user.upsert({
+    where: { email: 'bob@prisma.io' },
+    update: {},
+    create: {
+      email: 'bob@prisma.io',
+      name: 'Bob',
+      password: bcrypt.hashSync('password', 8),
+      posts: {
+        create: [
+          {
+            title: 'Follow Prisma on Twitter',
+            content: 'https://twitter.com/prisma',
+            published: true,
+          },
+          {
+            title: 'Follow Nexus on Twitter',
+            content: 'https://twitter.com/nexusgql',
+            published: true,
+          },
+        ],
+      },
+    },
+  });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
