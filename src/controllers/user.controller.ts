8d5d25e3bcf2230a1669182adf431dd12cba8a74
wenblack <wenblack012@gmail.com>
2023-04-14 18:00:45 -0300

    👨 Create UserCreate Route on UserController  👨

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,20 +1,45 @@
-import { NextFunction, Request, Response } from 'express';
-import { StatusCodes } from 'http-status-codes';
-import prisma from '../lib/prisma';
+import { NextFunction, Request, Response } from 'express'
+import { StatusCodes } from 'http-status-codes'
+import prisma from '../lib/prisma'
+import bcrypt from 'bcryptjs'
 
 class UserController {
   async getById(req: Request, res: Response, next: NextFunction) {
-    console.log(res.locals.payload); // just to see the payload
+    console.log(res.locals.payload) // just to see the payload
 
     const user = await prisma.user.findUnique({
       where: { id: Number(req.params.id) },
-    });
+    })
 
     if (!user)
-      return next({ status: StatusCodes.NOT_FOUND, message: 'User not found' });
+      return next({ status: StatusCodes.NOT_FOUND, message: 'User not found' })
 
-    res.status(StatusCodes.OK).json({ name: user.name, email: user.email });
+    res.status(StatusCodes.OK).json({ name: user.name, email: user.email })
+  }
+
+  async createUser(req: Request, res: Response, next: NextFunction) {
+    console.log(res.locals.payload) // just to see the payload
+    const { email, password, name, avatarUrl } = req.body
+
+    const userEmail = await prisma.user.findUnique({
+      where: { email: email }
+    })
+
+    const user = await prisma.user.create({
+      data: {
+        email: email,
+        password: bcrypt.hashSync(password, 8),
+        name: name,
+        //avatarUrl,
+      }
+    })
+
+    if (userEmail) {
+      return next({ status: StatusCodes.CONFLICT, message: 'Invali e-mail' })
+    }
+    else
+      res.status(StatusCodes.OK).json({ name: user.name, email: user.email })
   }
 }
 
-export default new UserController();
+export default new UserController()
--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,10 +1,14 @@
-import { Router } from 'express';
-import rescue from 'express-rescue';
-import userController from '../controllers/user.controller';
-import authMiddleware from '../middlewares/auth';
+import { NextFunction, Router } from 'express'
+import rescue from 'express-rescue'
+import userController from '../controllers/user.controller'
+import authMiddleware from '../middlewares/auth'
+import prisma from '../lib/prisma'
+import error from '../middlewares/error'
 
-const userRouter = Router();
+const userRouter = Router()
 
-userRouter.route('/:id').get(authMiddleware, rescue(userController.getById));
+userRouter.route('/:id').get(authMiddleware, rescue(userController.getById))
+userRouter.route('/create').post(rescue(userController.createUser))
 
-export default userRouter;
+
+export default userRouter
