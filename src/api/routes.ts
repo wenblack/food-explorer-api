2a9e6fe9d2bf5f6498dda65d9a3ec5e1e8b2fc6e
wenblack <wenblack012@gmail.com>
2023-04-14 22:31:01 -0300

    📁 Created Files route 📁

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,10 +1,12 @@
-import { Router } from 'express'
+import express, { Router } from 'express'
 import authRouter from '../routers/auth.router'
 import userRouter from '../routers/user.router'
+import { UPLOADS_FOLDER } from '../config/upload'
 
 const router = Router()
 
 router.use('/users', userRouter)
 router.use('/auth', authRouter)
+router.use("/files", express.static(UPLOADS_FOLDER))
 
 export default router
