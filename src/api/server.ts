7b5545e1846420b4d6271acc5193cf3176318efa
wenblack <wenblack012@gmail.com>
2023-06-28 20:18:07 -0300

    ✅ remove fixed port ✅

--- src/api/server.ts
+++ src/api/server.ts
@@ -2,5 +2,5 @@ import app from './app'
 const cors = require('cors')
 const PORT = process.env.PORT
 
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
+app.listen(() => console.log(`🚀 Server running`))
 app.use(cors())
